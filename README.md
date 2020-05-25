41ed19e3f6d5afef162fee8e360d0a4812e6d350
MertenNefs <mail@mertennefs.eu>
2020-05-25 20:29:39 +0200

    final

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# assignment_tidydata
+## Run Analysis scripts
+
+# run_analysis.R
+This R-script performs a data analysis on the original data found in "UCI HAR Dataset", findable in http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+The analysis concerns:
+- merging training and test data
+- selecting mean and standard deviation measurements
+- adding variable names
+- writing a tidy dataset, including the averages of the variables for each subject and variable, arranged in order
+
+# CodeBook.md
+This textfile explains the variables included in the tidy dataset, and how these relate to the original data
+
+# tidyData.txt
+Tabular datafile containing the tidy dataset, resulting from the R-script
--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+run_analysis<- function(){
+      
 ##Merges the training and the test sets to create one data set.
 trainValues <- read.table("UCI HAR Dataset/train/X_train.txt")
 trainLabels <- read.table("UCI HAR Dataset/train/y_train.txt")
@@ -7,6 +9,7 @@ testValues <- read.table("UCI HAR Dataset/test/X_test.txt")
 testLabels <- read.table("UCI HAR Dataset/test/y_test.txt")
 testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
+## adding a rownumber
 trainValues$rowname <- as.numeric(rownames(trainValues))
 trainLabels$rowname <- as.numeric(rownames(trainLabels))
 trainSubjects$rowname <- as.numeric(rownames(trainSubjects))
@@ -14,22 +17,22 @@ testValues$rowname <- as.numeric(rownames(testValues))
 testLabels$rowname <- as.numeric(rownames(testLabels))
 testSubjects$rowname <- as.numeric(rownames(testSubjects))
 
+##merging the training and teset data
 trainings <- merge(trainLabels, trainSubjects, by.x = "rowname",by.y = "rowname")
 trainData <- merge(trainings, trainValues, by.x = "rowname",by.y = "rowname")
 tests <- merge(testLabels, testSubjects, by.x = "rowname",by.y = "rowname")
 testData <- merge(tests, testValues, by.x = "rowname",by.y = "rowname")
 totalData <- rbind(trainData,testData)
 
-      
 ## Extracts only the measurements on the mean and standard deviation for each measurement.
 library(dplyr)
 features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
 key <- grep("mean|std", features$V2) ## makes vector containing variable names including 'mean' or 'std'
-keyFreq <- grep("Freq", features$V2) ##inlcuding Freqmean to take out
-key<- key[!key %in% keyFreq] ##eliminating the keyFreq from the vector
-      key1<- key+3
+remove <- grep("Freq|gravityMean", features$V2) ##excluding Freqmean and gravityMean
+key<- key[!key %in% remove] ##eliminating the removal vector from the key vector
+      key1<- key+3 ##compensating the vector for the three included columns
       key2<- append(key1,1:3,after=0) ##compensating the vector for the three included columns
-selectedData <- totalData[, key2]
+selectedData <- totalData[, key2] ##selected data
 
 
 ## Uses descriptive activity names to name the activities in the data set
@@ -45,12 +48,13 @@ selectedData$activity[selectedData$V1.x == 6] <- "laying"
 names <- features[key,2]
 names <- append(names, c("observation","activitynr","subject"), after=0)
 names <- append(names, "activity", after=69)
-colnames(selectedData)<- names
-
+colnames(selectedData)<- names ##full column headers vector
 
 ## creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-selectedData %>% group_by(subject,activity) -> groupedData
-summarize_all(groupedData,mean)->summarizedData
-arrange(summarizedData, subject,activitynr)-> summarizedData
-tidyData<- summarizedData[,-(3:4)]
-write.table(tidyData, "tidyData.txt", row.names = FALSE)
+selectedData %>% group_by(subject,activity) -> groupedData ##group data by subject + activity
+summarize_all(groupedData,mean)->summarizedData ##summarize grouped data - mean of all variables
+arrange(summarizedData, subject,activitynr)-> summarizedData ##arranging in ascending order
+tidyData<- summarizedData[,-(3:4)] ##selecting tidy data columns
+write.table(tidyData, "tidyData.txt", row.names = FALSE) ##writing tidy data file
+
+}
